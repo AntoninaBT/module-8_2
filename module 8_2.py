34260f33dc43c465b5c3ff77ab8cba4732e37f86
AntoninaBT <gorbaa533@gmail.com>
2024-12-15 15:51:59 +0300

    Add files via upload

---
+++ module 8_2.py
@@ -0,0 +1,11 @@
+# Teplova//
+def add_everything_up(a, b):
+    try:
+        if isinstance (a, (int, float, str)) and isinstance(b, (int, float, str)):
+            return a + b
+    except TypeError:
+        return f'{str(a)}{str(b)}'
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
